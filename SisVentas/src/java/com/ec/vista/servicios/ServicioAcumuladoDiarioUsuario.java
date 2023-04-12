cdd967143c8d25c477d45ca36025e87e76be60e7
Darwin <darwinvinicio14_11@hotmail.com>
2023-04-12 15:12:29 -0500

    Cambios reporte cierre y consulta cierre

--- SisVentas/src/java/com/ec/vista/servicios/ServicioAcumuladoDiarioUsuario.java
+++ SisVentas/src/java/com/ec/vista/servicios/ServicioAcumuladoDiarioUsuario.java
@@ -8,9 +8,13 @@ import com.ec.entidad.Usuario;
 import com.ec.servicio.HelperPersistencia;
 import com.ec.untilitario.ModeloAcumuladoDiaUsuario;
 import com.ec.vistas.AcumuladopordiaUsuario;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
@@ -32,6 +36,17 @@ public class ServicioAcumuladoDiarioUsuario {
 
     public List<ModeloAcumuladoDiaUsuario> findCierrePorUsuario(Date fecha, Usuario idUsuario) {
 
+        String pattern = "yyyy-MM-dd";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+
+        String date = simpleDateFormat.format(new Date());
+        Date fechaConsult = null;
+        try {
+            fechaConsult = simpleDateFormat.parse(date);
+        } catch (ParseException ex) {
+            Logger.getLogger(ServicioAcumuladoDiarioUsuario.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
         List<ModeloAcumuladoDiaUsuario> listaAcumuladopordiaUsuarios = new ArrayList<ModeloAcumuladoDiaUsuario>();
         try {
             em = HelperPersistencia.getEMF();
@@ -39,14 +54,14 @@ public class ServicioAcumuladoDiarioUsuario {
             String SQL = "SELECT new com.ec.untilitario.ModeloAcumuladoDiaUsuario(a.usuLogin,a.facTotal,a.totalntv,a.totalacumulado) FROM AcumuladopordiaUsuario a WHERE a.facFecha =:facFecha ";
             String WHERE = " AND a.idUsuario=:idUsuario";
             Query query = null;
-            if (idUsuario.getIdUsuario() == 1) {
-                query = em.createQuery(SQL);
-                query.setParameter("facFecha", fecha);
-            } else {
-                query = em.createQuery(SQL + WHERE);
-                query.setParameter("facFecha", fecha);
-                query.setParameter("idUsuario", idUsuario.getIdUsuario());
-            }
+//            if (idUsuario.getIdUsuario() == 1) {
+//                query = em.createQuery(SQL);
+//                query.setParameter("facFecha", fecha);
+//            } else {
+            query = em.createQuery(SQL + WHERE);
+            query.setParameter("facFecha", fechaConsult);
+            query.setParameter("idUsuario", idUsuario.getIdUsuario());
+//            }
 
             listaAcumuladopordiaUsuarios = (List<ModeloAcumuladoDiaUsuario>) query.getResultList();
             em.getTransaction().commit();
